24dad6ed11d4119f1d857f4563e89aed71d13eaf
Will Hickey <whicke01@gmail.com>
2021-05-04 15:32:27 -0400

    added guesttiles with editable names

--- app/javascript/react/components/ReceiptFormTiles.js
+++ app/javascript/react/components/ReceiptFormTiles.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react'
 
+import GuestTile from './GuestTile'
+
 const ReceiptFormTiles = (props) => {
   const [dinnerParty, setDinnerParty] = useState({
     restaurant: "",
-    party_size: 1
+    party_size: 1,
+    guests: []
   })
 
   const handleChange = (event) => {
@@ -31,52 +34,84 @@ const ReceiptFormTiles = (props) => {
     }
   }
 
+  const onEdit = (guest_id, name) => {
+    let tempGuests = dinnerParty.guests
+    tempGuests[guest_id] = name
+
+    setDinnerParty({
+      ...dinnerParty,
+      guests: tempGuests
+    })
+
+  }
+
   if(props.form_number === 1) {
     return(
       <>
-        <h3>This is the 1st form for getting number of guests and restaurant name</h3>
-        <p>Party size (how many people are splitting the bill):</p>
+        <h3>Party Size (how many people are splitting the bill):</h3>
         <div className='grid-x grid-margin-x align-spaced'>
-          <div className="cell small-10 input-group plus-minus-input">
-            <div className="input-group-button">
-              <button 
-                type="button" 
-                className="button hollow circle" 
-                data-quantity="minus" 
-                data-field="party_size"
-                onClick={smallButtonClick}
-                >
-                <i className="fa fa-minus" aria-hidden="true"></i>
-              </button>
-            </div>
-            
-            <input 
-              className="input-group-field" 
-              type="number" 
-              name="party_size" 
-              value={dinnerParty.party_size} 
-              onChange={handleChange}
-            />
-            <div className="input-group-button">
-              <button 
-                type="button" 
-                className="button hollow circle" 
-                data-quantity="plus" 
-                data-field="party_size"
-                onClick={smallButtonClick}
-                >
-                <i className="fa fa-plus" aria-hidden="true"></i>
-              </button>
-            </div>
-          </div>
-        </div>
+          <form onSubmit={props.onNextClick}>
+            <div className="cell small-10 input-group plus-minus-input">
+              <div className="input-group-button">
+                <button 
+                  type="button" 
+                  className="button hollow circle" 
+                  data-quantity="minus" 
+                  data-field="party_size"
+                  onClick={smallButtonClick}
+                  >
+                  <i className="fa fa-minus" aria-hidden="true"></i>
+                </button>
+              </div>
 
-        <button className='cell home_button' onClick={props.onNextClick}><h3>Next</h3></button>
+              <input 
+                className="input-group-field" 
+                type="number" 
+                name="party_size" 
+                value={dinnerParty.party_size} 
+                onChange={handleChange}
+              />
+
+              <div className="input-group-button">
+                <button 
+                  type="button" 
+                  className="button hollow circle" 
+                  data-quantity="plus" 
+                  data-field="party_size"
+                  onClick={smallButtonClick}
+                  >
+                  <i className="fa fa-plus" aria-hidden="true"></i>
+                </button>
+              </div>
+            </div>
+
+            <div>
+              <label className='rest_name_input' htmlFor='restaurant'> Restaurant name:
+                <input type='text' name='restaurant' value={dinnerParty.restaurant} onChange={handleChange}/>
+              </label>
+            </div>
+
+            <input type='submit' value='Next' className='home_button next_button'/>
+          </form>
+        </div>
       </>
     )
   } else if(props.form_number === 2) {
+    let guests = new Array(dinnerParty.party_size)
+
+    for( let i = 0; i < dinnerParty.party_size; i++ ) {
+      guests[i] = <GuestTile 
+        key={i+1}
+        id={i}
+        name={`Guest ${i+1}`} 
+        onEdit={onEdit}
+      />
+    }
+
     return(
-      <h3>This is the 2nd form for setting Guest's names, {dinnerParty.party_size}</h3>
+      <>
+        {guests}
+      </>
     )
   } else if(props.form_number === 3) {
     return(
