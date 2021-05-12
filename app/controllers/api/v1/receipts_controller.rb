baa6e935db0c4bb4bd8979852fe7697d989a8cee
Will Hickey <whicke01@gmail.com>
2021-05-12 15:16:08 -0400

    removed a pry from api controller

--- app/controllers/api/v1/receipts_controller.rb
+++ app/controllers/api/v1/receipts_controller.rb
@@ -3,7 +3,6 @@ class Api::V1::ReceiptsController < ApplicationController
   def create
     submittedData = receipt_params()
     receipt = Receipt.new(restaurant: submittedData[:restaurant], tax: 0.00, receipt_url: submittedData[:image], receipt_text: 'this will be edited later by the return value of the google OCR api')
-    binding.pry
     render json: receipt
     
   end
